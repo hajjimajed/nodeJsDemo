fa9d1f854fe9ef4d3c1c60751bbd8ea3b57d258e
hajjimajed <hajjimajed78@gmail.com>
2023-04-25 22:12:35 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+
+    const url = req.url;
+
+    if (url === '/') {
+        res.write('<html>')
+        res.write('<head><title>first app</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>')
+        res.write('</html>')
+        return res.end();
+    }
+    if (url === '/message' && req.method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            // console.log('parsedBody', parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        })
+
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>first app</title></head>')
+    res.write('<body><h1>my first app</h1></body>')
+    res.write('</html>')
+    res.end();
+});
+
+server.listen(8000);
