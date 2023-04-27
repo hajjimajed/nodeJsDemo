2166567031dea2c82b689f8b83ea48f99964e82f
hajjimajed <hajjimajed78@gmail.com>
2023-04-27 17:19:42 +0100

    converting html pages to pug

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const path = require('path');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //     next();
 // });
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 
 app.use(shopRoutes);
 
--- routes/shop.js
+++ routes/shop.js
@@ -5,10 +5,13 @@ const router = express.Router();
 const path = require('path');
 
 const rootDir = require('../utils/path');
+const adminData = require('./admin');
 
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    const products = adminData.products;
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    res.render('shop', { prods: products, docTitle: 'shop' })
 });
 
 module.exports = router;
