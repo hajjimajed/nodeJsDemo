792fc98561e18fcc8b3d84f1e4c47aad97ac9e89
hajjimajed <hajjimajed78@gmail.com>
2023-04-28 13:57:32 +0100

    converting to ejs

--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,7 @@ const adminData = require('./admin');
 router.get('/', (req, res, next) => {
     const products = adminData.products;
     // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
-    res.render('shop', { prods: products, docTitle: 'shop', path: '/' })
+    res.render('shop', { prods: products, pageTitle: 'shop', path: '/' })
 });
 
 module.exports = router;
