228d9f512554658ec93f603a1da7a70f61209632
hajjimajed <hajjimajed78@gmail.com>
2023-05-03 17:16:26 +0100

    adding dynamic link to product detail and initialize add to cart funcionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -11,7 +11,18 @@ exports.getProducts = (req, res, next) => {
             path: '/products'
         });
     });
+}
 
+
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        res.render('shop/product-detail', {
+            product: product,
+            pageTitle: product.title,
+            path: '/products'
+        })
+    })
 }
 
 
@@ -31,6 +42,13 @@ exports.getCart = (req, res, next) => {
         path: '/cart',
         pageTitle: 'Your Cart'
     })
+}
+
+
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    console.log(prodId);
+    res.redirect('/cart')
 }
 
 
