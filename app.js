b8876198a6d991fc06bf374ce7ca024562059ef1
hajjimajed <hajjimajed78@gmail.com>
2023-05-06 16:43:02 +0100

    changing from sql mysql2 to nosql mongodb atlas

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const path = require('path');
-
 const app = express();
 
+// using sql database : mysql2
+// const sequelize = require('./utils/database');
+// const Product = require('./models/product');
+// const User = require('./models/user');
+// const Cart = require('./models/cart');
+// const CartItem = require('./models/cart-item');
+// const Order = require('./models/order');
+// const OrderItem = require('./models/order-item');
 
-const sequelize = require('./utils/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+
+const mongoConnect = require('./utils/database');
 
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
+
 const notFoundController = require('./controllers/error');
 
 
-// db.execute('SELECT * FROM products')
-//     .then(result => {
-//         console.log(result[0]);
-//     })
-//     .catch(err => {
-//         console.log(err);
-//     });
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,58 +32,65 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findByPk(1)
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => console.log(err))
+    // User.findByPk(1)
+    //     .then(user => {
+    //         req.user = user;
+    //         next();
+    //     })
+    //     .catch(err => console.log(err))
 })
 
-// app.use((req, res, next) => {
-//     console.log('this always runs')
-//     next();
-// });
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
+
 app.use(notFoundController.notFound)
 
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem })
 
-sequelize
-    // .sync({ force: true })
-    .sync()
-    .then(result => {
-        return User.findByPk(1)
-        // console.log(result);
-        app.listen(8000);
-    })
-    .then(user => {
-        if (!user) {
-            return User.create({ name: 'majed', email: 'majed@gmail.com' });
-        }
-        return user;
-    })
-    .then(user => {
-        // console.log(user);
-        user.createCart();
-    })
-    .then(cart => {
-        app.listen(8000);
-    })
-    .catch(err => console.log(err))
+mongoConnect((client) => {
+    console.log(client);
+    app.listen(8000);
+})
 
 
 
-// const server = http.createServer(app);
+
+
+
+// using sql database : mysql2
+// Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+// User.hasMany(Product);
+// User.hasOne(Cart);
+// Cart.belongsTo(User);
+// Cart.belongsToMany(Product, { through: CartItem });
+// Product.belongsToMany(Cart, { through: CartItem });
+// Order.belongsTo(User);
+// User.hasMany(Order);
+// Order.belongsToMany(Product, { through: OrderItem })
+
+// sequelize
+//     // .sync({ force: true })
+//     .sync()
+//     .then(result => {
+//         return User.findByPk(1)
+//         // console.log(result);
+//         app.listen(8000);
+//     })
+//     .then(user => {
+//         if (!user) {
+//             return User.create({ name: 'majed', email: 'majed@gmail.com' });
+//         }
+//         return user;
+//     })
+//     .then(user => {
+//         // console.log(user);
+//         user.createCart();
+//     })
+//     .then(cart => {
+//         app.listen(8000);
+//     })
+//     .catch(err => console.log(err))
+
 
--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,38 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://hajjimajed78:rHh6z3McFzWLIPbK@cluster0.xh4x4zf.mongodb.net/?retryWrites=true&w=majority')
+        .then(client => {
+            console.log('connected !');
+            callback(client);
+        })
+        .catch(err => console.log(err))
+}
+
+module.exports = mongoConnect;
 
 
-const sequelize = new Sequelize('node-complete', 'root', '12345678', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
 
 
-module.exports = sequelize;
+
+
+
+
+
+
+
+
+
+
+// using sql database : mysql2 and sequelize
+// const Sequelize = require('sequelize');
+
+
+// const sequelize = new Sequelize('node-complete', 'root', '12345678', {
+//     dialect: 'mysql',
+//     host: 'localhost'
+// });
+
+
+// module.exports = sequelize;
