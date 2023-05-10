cb86a81b81a1626bb9500979bf51f876217ac78d
hajjimajed <hajjimajed78@gmail.com>
2023-05-10 11:18:23 +0100

    adding sending emails to users functionality with nodemailer and sendgrid

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,21 @@
+const crypto = require('crypto');
+
 const bcrypt = require('bcryptjs');
 
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+
 const User = require('../models/user');
+
+
+const transporter = nodemailer.createTransport(
+    sendgridTransport({
+        auth: {
+            api_key: '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'
+        }
+    })
+);
+
 
 exports.getLogin = (req, res, next) => {
     let message = req.flash('error');
@@ -83,6 +98,15 @@ exports.postSignup = (req, res, next) => {
                 })
                 .then(result => {
                     res.redirect('/login');
+                    return transporter.sendMail({
+                        to: email,
+                        from: 'hajjimajed78@gmail.com',
+                        subject: 'Signup succeeded!',
+                        html: '<h1>You successfully signed up!</h1>'
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
                 });
         })
 
@@ -95,3 +119,52 @@ exports.postLogout = (req, res, next) => {
         res.redirect('/');
     });
 };
+
+
+exports.getReset = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
+    res.render('auth/reset', {
+        path: '/reset',
+        pageTitle: 'Reset Password',
+        errorMessage: message
+    });
+};
+
+
+exports.postReset = (req, res, next) => {
+    crypto.randomBytes(32, (err, buffer) => {
+        if (err) {
+            console.log(err);
+            return res.redirect('/reset');
+        }
+        const token = buffer.toString('hex');
+        User.findOne({ email: req.body.email })
+            .then(user => {
+                if (!user) {
+                    req.flash('No account relatd to this email address');
+                    return res.redirect('/reset')
+                }
+                user.resetToken = token;
+                user.resetTokenExpiration = Date.now() + 3600000;
+                return user.save();
+            })
+            .then(result => {
+                res.redirect('/');
+                transporter.sendMail({
+                    to: req.body.email,
+                    from: 'hajjimajed78@gmail.com',
+                    subject: 'password reset',
+                    html: `
+                <p>You requested a password reset</p>
+                <p>Click this <a href="http://localhost:8000/reset/${token}">link</a> to set a new password</p>
+                `
+                })
+            })
+            .catch(err => console.log(err))
+    })
+}
