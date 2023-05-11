64857d50d10e623ee20559aa96b060a936da3140
hajjimajed <hajjimajed78@gmail.com>
2023-05-11 16:46:12 +0100

    adding downloading file functionlaity

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,9 @@
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const fs = require('fs');
+const path = require('path');
+
 
 exports.getProducts = (req, res, next) => {
     Product.find()
@@ -124,4 +127,16 @@ exports.getOrders = (req, res, next) => {
 };
 
 
-
+exports.getInvoice = (req, res, next) => {
+    const orderId = req.params.orderId;
+    const invoiceName = 'invoice-' + orderId + '.pdf';
+    const invoicePath = path.join('data', 'invoices', invoiceName);
+    fs.readFile(invoicePath, (err, data) => {
+        if (err) {
+            console.log(err)
+        }
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'inline; filename = "' + invoiceName + '"');
+        res.send(data);
+    });
+};
